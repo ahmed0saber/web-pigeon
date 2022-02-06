c4cbb3a6f2ee7893390f64f0e9efcca8588799da
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-06 03:08:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*  FAV OR REC  */
+/*  FAV or REC  */
 function show_fav(){
     document.getElementsByClassName('messages-container')[0].style.display = 'none';
     document.getElementsByClassName('fav-messages-container')[0].style.display = 'flex';
